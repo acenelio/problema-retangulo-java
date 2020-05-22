ab47eb1d2f6a7c757d665b1f4bdbb5a69c6bd886
Nelio Alves <acenelio@gmail.com>
2020-05-22 19:37:38 -0300

    Correcao: declaracao no primeiro uso

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,10 +10,8 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double largura;
-		
 		System.out.print("Digite a largura do retangulo: ");
-		largura = sc.nextDouble();
+		double largura = sc.nextDouble();
 		
 		while (largura <= 0.0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
